d896ce0a4954990ffd4586b2810d10b45248d968
Busayor <35084577+Iambusayor@users.noreply.github.com>
2023-08-26 02:21:27 +0100

    Create extract_annotations.py
    file to extract annotations from IO Annotator export

---
+++ extract_annotations.py
@@ -0,0 +1,62 @@
+import json
+import os
+import argparse
+from collections import defaultdict
+from tqdm import tqdm
+
+def find_tag(specific_token, dict_tags):
+    # The find_tag function remains the same
+    pass
+
+def extract_from_json(input_path, lang):
+    with open(input_path) as f:
+        dict_ann = json.loads(f.read())
+
+    per_ann_sentences = {}
+    for s, dict_sent in enumerate(dict_ann['annotations']):
+        content = dict_sent['text_snippet']['content']
+        reference = int(dict_sent['reference'])
+        tokens_annot = dict_sent['annotations']
+        start_end_offset = {}
+        for dict_token in tokens_annot:
+            start_idx = dict_token['text_extraction']['text_segment']['start_offset']
+            end_idx = dict_token['text_extraction']['text_segment']['end_offset']
+            start_end_str = '_'.join([str(i) for i in range(start_idx, end_idx)])
+            start_end_offset[start_end_str] = dict_token['display_name']
+
+        start_offsets = sorted(start_end_offset.keys())
+
+        tokens = content.split()
+        beg_idx = 0
+        annotated_words = []
+        for tok in tokens:
+            tok_start_end_str = '_'.join([str(i) for i in range(beg_idx, beg_idx + len(tok))])
+            tag = find_tag(tok_start_end_str, start_end_offset)
+            annotated_words.append((tok, tag))
+            beg_idx += len(tok)+1
+
+        per_ann_sentences[reference] = annotated_words
+
+    output_file = lang + '.tsv'
+
+    with open(output_file, 'w') as f:
+        for reference, annotations in tqdm(per_ann_sentences.items(), desc=f'Writing data into {output_file}', total=len(per_ann_sentences)):
+            for word, tag in annotations:
+                f.write(f"{word}\t{tag}\n")
+            f.write('\n')
+    return "Extraction done"
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Extract NER data from JSON annotations.')
+    parser.add_argument('--input_path', type=str, help='Path to the input JSON file')
+    parser.add_argument('--output_path', type=str, help='Path to the output TSV file')
+
+    args = parser.parse_args()
+
+    input_path = args.input_path
+    output_path = args.output_path
+
+    lang = os.path.splitext(os.path.basename(input_path))[0]
+    extract_from_json(input_path, output_path)
+
+## python script_name.py --input_path input.json --output_path output.tsv
