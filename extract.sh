18fae2b00b00550a01f2a1a8a4acc8c844faae0f
iambusayor <hawobade@gmail.com>
2023-08-30 10:58:20 +0100

    removed redundant run and extract sh files

--- extract.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Set the paths to your input JSON file and desired output TSV file
-input_path="/path/to/your/input.json"
-output_path="/path/to/your/output.tsv"
-
-# Run the Python script
-python extract_annotations.py --input_path "$input_path" --output_path "$output_path"
